53a1777c98aae2ca98286962a9758af473ed56f1
tolunayg <tolunaygedikoglu@gmail.com>
2023-04-09 23:08:47 +0300

    Index html path fixed to serve frontend on elastic beanstalk

--- app.js
+++ app.js
@@ -4,12 +4,18 @@ const router = require('./routes/router');
 const path = require('path');
 
 app.use(express.json()); // Parse JSON data from requests
+
 app.use('/api', router); // Mount the router middleware at the '/api' path
-app.use(express.static(path.join(__dirname, 'client/build'))); // Serve static files
-app.get('*', (req, res) => { // Handle all other routes with the React app
-  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+
+// Serve static files from the client/build directory
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+// Serve the index.html file for any other requests
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
+
 const port = process.env.PORT || 5000;
-app.listen(process.env.PORT || 5000, () => {
-    console.log(`Server listening on port ${process.env.PORT || 5000}`);
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
 });
