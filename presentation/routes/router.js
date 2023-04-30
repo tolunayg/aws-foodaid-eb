fc159dbc197f619540982da1dae3a2852bc3c6a9
Erdem <35002668+cengerdem@users.noreply.github.com>
2023-05-01 00:20:57 +0300

    add product endpoints

--- presentation/routes/router.js
+++ presentation/routes/router.js
@@ -1,17 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const restaurantRoutes = require('./restaurants');
-
-// Middleware function to set req.user
-// const setUser = (req, res, next) => {
-//   req.user = req.oidc.user;
-//   next();
-// };
+const productRoutes = require('./products');
 
 // Attach the middleware function to the router
 // router.use(setUser);
 
 // Mount your route files to the router object
 router.use('/restaurants', restaurantRoutes);
+router.use('/products', productRoutes);
 
 module.exports = router;
