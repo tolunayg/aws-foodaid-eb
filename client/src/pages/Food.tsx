b23bd3c019908a5cb880987b0c999634c8dabfa1
efaefas <96652404+efaefas@users.noreply.github.com>
2023-05-04 22:15:46 +0300

    form component addition

--- client/src/pages/Food.tsx
+++ client/src/pages/Food.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import Form from '../components/Forms/locationselectform'
+import MyComponent from '../components/Forms/foodform'
 
 function Food() {
   return (
     <>
         <div>Food</div>
+      
+      <Form/>
+       
+     
+        <MyComponent/> 
+      
     </>
   )
 }
