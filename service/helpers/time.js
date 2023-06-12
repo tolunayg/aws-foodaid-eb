efc3b675f7ac0f776e6df77344a7d78fff6e45ca
Erdem <35002668+cengerdem@users.noreply.github.com>
2023-06-12 20:55:52 +0300

    datefix

--- service/helpers/time.js
+++ service/helpers/time.js
@@ -1,4 +1,3 @@
-const now = new Date();
-const nowFormatted = `${now.getFullYear()}-${(now.getMonth() + 1).toString().padStart(2, '0')}-${now.getDate().toString().padStart(2, '0')}T${now.getHours().toString().padStart(2, '0')}:${now.getMinutes().toString().padStart(2, '0')}:${now.getSeconds().toString().padStart(2, '0')}.${now.getMilliseconds().toString().padStart(3, '0')}`
+var nowFormatted = `${new Date().getFullYear()}-${(new Date().getMonth() + 1).toString().padStart(2, '0')}-${new Date().getDate().toString().padStart(2, '0')}T${new Date().getHours().toString().padStart(2, '0')}:${new Date().getMinutes().toString().padStart(2, '0')}:${new Date().getSeconds().toString().padStart(2, '0')}.${new Date().getMilliseconds().toString().padStart(3, '0')}`
 
 module.exports = { nowFormatted }
